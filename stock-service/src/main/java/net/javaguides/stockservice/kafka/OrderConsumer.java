4e1d395e0f661b5980b67076d075ec41f89250f3
manjuram07 <khmanjunatha405@gmail.com>
2026-06-16 15:44:09 +0530

    adding kafka features to place the order and reduce the quantity

--- stock-service/src/main/java/net/javaguides/stockservice/kafka/OrderConsumer.java
+++ stock-service/src/main/java/net/javaguides/stockservice/kafka/OrderConsumer.java
@@ -1,25 +1,24 @@
 package net.javaguides.stockservice.kafka;
 
-import net.javaguides.basedomains.dto.OrderEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import net.javaguides.basedomains.dto.OrderCreatedEvent;
+import net.javaguides.stockservice.service.ProductService;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
+@RequiredArgsConstructor
 public class OrderConsumer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+    private final ProductService productService;
 
     @KafkaListener(
-            topics = "${spring.kafka.topic.name}",
-            groupId = "${spring.kafka.consumer.group-id}"
-    )
-    public void consume(OrderEvent event){
-        LOGGER.info("Order event received in stock service => {}", event.toString());
+            topics = "order-created",
+            groupId = "product-group")
+    public void consume(OrderCreatedEvent event) {
 
-        // save the order event into the database
+        System.out.println("Received event : " + event);
 
-
+        productService.reduceQuantity(event.getProductId(), event.getQuantity());
     }
 }
